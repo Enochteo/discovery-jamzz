70a3400161455fba4495a7d0b1e59e0b10e09dff
Enochteo <enochowoade@gmail.com>
2025-09-28 00:04:34 -0500

    add configured dotenv

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import eventRouter from './routes/events.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import './config/dotenv.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
